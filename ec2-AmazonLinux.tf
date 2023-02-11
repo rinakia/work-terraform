d5e4c3fae9054e5192eb40523a221b32bb60d993
tadaaki <tadaaki@cac.co.jp>
2023-02-11 16:28:22 +0900

    add EC2 setting

--- ec2-AmazonLinux.tf
+++ ec2-AmazonLinux.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "test" {
   ami           = "ami-06ee4e2261a4dc5c3"
   instance_type = "t3.micro"
+  subnet_id     = "subnet-058ae71eca7c94268"
+  key_name      = "tada-ppk"
+  vpc_security_group_ids = [
+    "sg-05cbb847323ab71af"
+    ]
 
   tags = {
     Name = "tf_test_AmazonLinux2"
